fb4a884ae6de3b7b7d9be27c2fb9c3770c9e487d
Chris LoJacono <clojacono92@gmail.com>
2021-03-12 12:13:23 -0600

    solution 2

--- CodeWarsSolutions/March2021.cs
+++ CodeWarsSolutions/March2021.cs
@@ -28,3 +28,27 @@ public class DnaStrand
           return builder.ToString();
         }
     }
+
+
+/* Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+[10, 343445353, 3453445, 3453545353453] should return 3453455. */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public static class Kata
+{
+	public static int sumTwoSmallestNumbers(int[] numbers)
+	{
+		var smallestFirst = numbers.Min();
+    numbers = numbers.Where((source) => source != smallestFirst).ToArray();
+    var secondNum = numbers.Min();
+    return smallestFirst + secondNum;
+	}
+}
