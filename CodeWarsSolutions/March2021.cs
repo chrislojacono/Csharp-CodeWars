a68e897f792e6eb79c2a72de6c85fc4bd8eed650
Chris LoJacono <clojacono92@gmail.com>
2021-03-12 15:46:08 -0600

    that was a hard one

--- CodeWarsSolutions/March2021.cs
+++ CodeWarsSolutions/March2021.cs
@@ -259,3 +259,19 @@ public class Kata
     return str.ToLower().Distinct().Count()==str.Length;
   }
 }
+
+/* Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. */
+using System;
+using System.Linq;
+
+public class Kata
+{
+  public static int SquareDigits(int n)
+  {
+    var final = Convert.ToString(n).Select((x) => Convert.ToInt32(Char.GetNumericValue(x)) * Convert.ToInt32(Char.GetNumericValue(x)));
+    var finalString = string.Join("", final);
+    return Convert.ToInt32(finalString);
+  }
+}
