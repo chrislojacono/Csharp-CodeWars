1ce347afedbd5e02a0d5be9b1cf2154fb96766fa
Chris LoJacono <clojacono92@gmail.com>
2021-03-12 12:35:02 -0600

    typing

--- CodeWarsSolutions/March2021.cs
+++ CodeWarsSolutions/March2021.cs
@@ -101,3 +101,10 @@ namespace Solution
     }
   }
 }
+
+
+/*
+Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+It should remove all values from list a, which are present in list b.
+*/
