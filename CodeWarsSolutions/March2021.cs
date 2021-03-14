097172d1e1f0b85b3c857ac3bf7476b40bc61934
Chris LoJacono <clojacono92@gmail.com>
2021-03-14 16:25:39 -0500

    solution

--- CodeWarsSolutions/March2021.cs
+++ CodeWarsSolutions/March2021.cs
@@ -316,3 +316,20 @@ public class Kata
 }
 
 // Upto and including n, this function will return the sum of all multiples of 3 and 5.
+
+namespace Solution
+{
+  public static class Program
+  {
+    public static int findSum(int n)
+    {
+      var sum = 0;
+      for(var i = 1; i <= n; i++){
+        if(i % 3 == 0 || i % 5 == 0){
+          sum += i;
+        }
+      }
+      return sum;
+    }
+  }
+}
