83338b79201dffa3d3eb623ce571b0118ccf2d66
Chris LoJacono <clojacono92@gmail.com>
2021-03-14 14:46:41 -0500

    more codewars

--- CodeWarsSolutions/March2021.cs
+++ CodeWarsSolutions/March2021.cs
@@ -291,3 +291,26 @@ public class MinMax
         return finalArray;
     }
 }
+
+/*
+Mr. Scrooge has a sum of money 'P' that he wants to invest. Before he does, he wants to know how many years 'Y' this sum 'P' has to be kept in the bank in order for it to amount to a desired sum of money 'D'.
+
+The sum is kept for 'Y' years in the bank where interest 'I' is paid yearly. After paying taxes 'T' for the year the new sum is re-invested.
+
+Note to Tax: not the invested principal is taxed, but only the year's accrued interest */
+
+using System;
+
+public class Kata
+{
+  public static int CalculateYears(double principal, double interest,  double tax, double desiredPrincipal)
+        {
+          var years = 0;
+          var principalGain = principal;
+          for(var i = 0; principalGain < desiredPrincipal; i++){
+            principalGain += (principalGain * interest) - ((principalGain * interest) * tax);
+            years++;
+          }
+          return years;
+        }
+}
