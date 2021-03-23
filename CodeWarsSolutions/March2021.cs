8bdee17f8b20bff17220fdd523cda1e92d8c7811
Chris LoJacono <clojacono92@gmail.com>
2021-03-23 15:32:29 -0500

    store

--- CodeWarsSolutions/March2021.cs
+++ CodeWarsSolutions/March2021.cs
@@ -370,3 +370,27 @@ public static class JadenCase
     return String.Join(" ", finalList);
   }
 }
+
+/*
+The rgb function is incomplete. Complete it so that passing in RGB decimal values will result in a hexadecimal representation being returned. Valid decimal values for RGB are 0 - 255. Any values that fall out of that range must be rounded to the closest valid value.
+
+Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
+
+The following are examples of expected output values:*/
+
+function componentToHex(c) {
+    if(c > 255){
+    return "FF"
+  }
+  var hex = c.toString(16);
+  if(hex < 0){
+    return "00"
+  }
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+
+
+function rgb(r, g, b){
+  return  componentToHex(r).toUpperCase() + componentToHex(g).toUpperCase() + componentToHex(b).toUpperCase();
+}
