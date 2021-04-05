b7ac2210f8e41e0582d8b979eb727da9ff3927c0
Chris LoJacono <clojacono92@gmail.com>
2021-04-05 18:00:29 -0500

    one question done

--- CodeWarsSolutions/April2021.cs
+++ CodeWarsSolutions/April2021.cs
@@ -1,1 +1,14 @@
 //APRIL C# SOLUTIONS!!
+
+//Given an integer as input, can you round it to the next (meaning, "higher") multiple of 5?
+
+public class Kata
+{
+  public static int RoundToNext5(int n)
+  {
+    while(n % 5 != 0){
+      n++;
+    }
+    return n;
+  }
+}
