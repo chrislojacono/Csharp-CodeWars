45a64c0a5bf62b3200f767e08eacc3ae24be4cf1
Chris LoJacono <clojacono92@gmail.com>
2021-07-06 11:43:42 -0500

    sql stuff

--- CodeWarsSolutions/leetcode.sql
+++ CodeWarsSolutions/leetcode.sql
@@ -1,1 +1,5 @@
 /* Write a SQL query for a report that provides the following information for each person in the Person table, regardless if there is an address for each of those people:*/
+Select P.FirstName, P.LastName, A.City, A.State
+From Person P
+        left Join Address A
+        on (P.PersonId = A.PersonId)
