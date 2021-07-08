eea755579f496300715edf2dd124a6f56314465b
Chris LoJacono <clojacono92@gmail.com>
2021-07-08 10:01:09 -0500

    save

--- CodeWarsSolutions/leetcode.sql
+++ CodeWarsSolutions/leetcode.sql
@@ -14,3 +14,5 @@ Select Email
 From Person
 Group by Email
 Having Count(Email) > 1
+
+/* Given the Employee table, write a SQL query that finds out employees who earn more than their managers. For the above table, Joe is the only employee who earns more than his manager.*/
